4b5f0120e2ab16abd600113701e46b319450c562
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-04-11 19:35:53 +0200

    Encapsulate methods of Sudoku type

--- main.py
+++ main.py
@@ -69,36 +69,43 @@ class Sudoku:
       self.cells,
       0)
 
+  def solve(self):
+    for column in self.__columns():
+      column.discard_used_values()
+    for row in self.__rows():
+      row.discard_used_values()
+    self.__calculate_values()
+  
+  def print(self):
+    for row in self.cells:
+      line = "".join(map(lambda x: str(x), row))
+      print("|" + line + "|\n")
+
   # Returns column of specific index (zero based)
-  def column(self, index):
+  def __column(self, index):
     cells = []
     for ri in range(0, 9):
       cells.append(self.cells[ri][index])
     return Slice(cells)
 
   # Returns all columns
-  def columns(self):
+  def __columns(self):
     for ci in range(0, 9):
-      yield self.column(ci)
+      yield self.__column(ci)
 
   # Returns row of specific index (zero based)
-  def row(self, index):
+  def __row(self, index):
     return Slice(self.cells[index])
 
   # Returns all rows
-  def rows(self):
+  def __rows(self):
     for ri in range(0, 9):
-      yield self.row(ri)
+      yield self.__row(ri)
 
-  def calculate_values(self):
+  def __calculate_values(self):
     for row in self.cells:
       for cell in row:
         cell.calculate_value()
-  
-  def print(self):
-    for row in self.cells:
-      line = "".join(map(lambda x: str(x), row))
-      print("|" + line + "|\n")
 
 class SudokuFromFile:
   def __init__(self, file_name):
@@ -123,13 +130,6 @@ class SudokuFromFile:
     return sudoku
 
 sudoku = SudokuFromFile("sudoku_3_missing.txt").load()
-
-for column in sudoku.columns():
-  column.discard_used_values()
-for row in sudoku.rows():
-  row.discard_used_values()
-
-sudoku.calculate_values()
-
+sudoku.solve()
 sudoku.print()
 print(sudoku.number_of_known_values())
