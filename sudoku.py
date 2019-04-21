a5cd0b1120af0d5dabaa3b1b312f4bbac766fda8
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-04-21 17:08:20 +0200

    Extract type Slice from Sudoku

--- sudoku.py
+++ sudoku.py
@@ -23,7 +23,7 @@ class Sudoku:
     row = self.row(ri)
     column = self.column(ci)
     square = self.square(ri, ci)
-    valid = self.__valid(row) and self.__valid(column) and self.__valid(square)
+    valid = row.valid() and column.valid() and square.valid()
 
     if(valid):
       if(ri == 8 and ci == 8):
@@ -40,11 +40,11 @@ class Sudoku:
     cells = []
     for ri in range(0, 9):
       cells.append(self.cells[ri][index])
-    return cells
+    return Slice(cells)
 
   # Returns row of specific index (zero based)
   def row(self, index):
-    return self.cells[index]
+    return Slice(self.cells[index])
 
   # Return 3x3 square of sudoku containing cell of specific index
   # ri - row index <1, 9>
@@ -56,15 +56,18 @@ class Sudoku:
     for ri in range(0, 3):
       for ci in range(0,3):
         cells.append(self.cells[row * 3 + ri][column * 3 + ci])
-    return cells
+    return Slice(cells)
 
-  def __valid(self, slice):
-    values = [0] * 9
-    for cell in slice:
-      if(cell == None):
+class Slice:
+  def __init__(self, values):
+    self.__values = values
+
+  def valid(self):
+    occurences = [0] * 9
+    for value in self.__values:
+      if(value == None):
         continue
-      index = cell - 1
-      if(values[index] == 1):
+      if(occurences[value - 1] == 1):
         return False
-      values[index] = 1
+      occurences[value - 1] = 1
     return True
