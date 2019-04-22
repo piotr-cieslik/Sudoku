96129805b58a1fe477df24867ede69c566842a1c
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-04-22 11:36:53 +0200

    Algorithm iteration goes only throught empty cells

--- backtracking.py
+++ backtracking.py
@@ -9,34 +9,34 @@ class Backtracking:
   # Method tries to solves sudoku.
   # Returns true when solution was found or false otherwise.
   def solve(self):
+    (ri, ci) = self.first_empty_cell()
     for x in range(1, 10):
-      if(self.__iteration(0, 0, x)):
+      if(self.__iteration(ri, ci, x)):
         return True
     return False
 
   # Single iteration of algorithm.
   # It sets specified value and check if sudoku is still valid.
-  # If yes, it tries to solve value of next cell.
+  # If yes, it tries to solve value of next empty cell.
   # If no, it leaves cell unset and return false.
   # Parameters:
   #   ri - row index of current cell
   #   ci - column index of current cell
   #   value - value of current cell
-  def __iteration(self, ri, ci, value):
-    # Calculate position of next cell
-    ci_next = ci if ri < 8 else ci + 1
-    ri_next = ri + 1 if ri < 8 else 0
-
-    # If current cell has value, skip it.
-    if(self.sudoku.cells[ri][ci] != None):
-      return self.__iteration(ri_next, ci_next, value)
-    
+  def __iteration(self, ri, ci, value):   
     # Assign value to cell and verify correctness.
     self.sudoku.cells[ri][ci] = value
 
     # If sudoku is valid, try to solve next value.
-    if(self.sudoku.row(ri).valid() and self.sudoku.column(ci).valid() and self.sudoku.block(ri, ci).valid()):
-      if(ri == 8 and ci == 8):
+    valid =\
+      self.sudoku.row(ri).valid() and\
+      self.sudoku.column(ci).valid() and\
+      self.sudoku.block(ri, ci).valid()
+    if(valid):
+      (ri_next, ci_next) = self.next_empty_cell(ri, ci)
+      # Check if next empty cell exist.
+      # If not, returns true.
+      if(ri_next == None):
         return True
       for x in range(1, 10):
         if(self.__iteration(ri_next, ci_next, x)):
@@ -45,3 +45,21 @@ class Backtracking:
     # If sudoku is not valid, clear value of the cell and return false.
     self.sudoku.cells[ri][ci] = None
     return False
+
+  def first_empty_cell(self):
+    if(self.sudoku.cells[0][0] == None):
+      return (0, 0)
+    return self.next_empty_cell(0, 0)
+
+  def next_empty_cell(self, ri, ci):
+    # End of sudoku board.
+    if(ri == 8 and ci == 8):
+      return (None, None)
+    # Calculate position of next cell
+    ci_next = ci if ri < 8 else ci + 1
+    ri_next = ri + 1 if ri < 8 else 0
+    # Check value of next cell.
+    # If value is not set, returns position of it.
+    if(self.sudoku.cells[ri_next][ci_next] == None):
+      return (ri_next, ci_next)
+    return self.next_empty_cell(ri_next, ci_next)
